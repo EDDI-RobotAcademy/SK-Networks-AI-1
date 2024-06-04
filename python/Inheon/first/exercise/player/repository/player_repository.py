382da53dfb98e54c561dc96fc7194df53483c0d3
Inheon Choi <cih956964@gmail.com>
2024-06-04 12:15:25 +0900

    feat: [최인헌] player domain update [SKAIPx]
    [미완]
    [Attribute Error] AttributeError: 'NoneType' object has no attribute 'getPlayerId'

---
+++ python/Inheon/first/exercise/player/repository/player_repository.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class PlayerRepository(ABC):
+    @abstractmethod
+    def create(self, nickname):
+        pass
+
+    @abstractmethod
+    def findPlayerByNickname(self, nickname):
+        pass
