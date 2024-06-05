32ee64368968531c2a039e747160ddd6910959de
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-06-05 19:59:51 +0900

    feat [이호준] Product 리스트를 생성하고 출력할 수 있습니다. [THIRD-EXER-1]

---
+++ python/HojoonLee/exercise3/product/product/service/product_service.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class ProductService(ABC):
+
+    @abstractmethod
+    def getProductList(self):
+        pass
+    @abstractmethod
+    def displayProductList(self):
+        pass
