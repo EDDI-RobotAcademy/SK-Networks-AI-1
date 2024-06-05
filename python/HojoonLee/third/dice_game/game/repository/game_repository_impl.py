8b8fb7ac8ec17012f9bf307f6eb5d94621cc9977
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-06-05 14:23:31 +0900

    feat (이호준) 주사위게임확장 [SKAIx]

--- python/HojoonLee/third/dice_game/game/repository/game_repository_impl.py
+++ python/HojoonLee/third/dice_game/game/repository/game_repository_impl.py
@@ -18,3 +18,17 @@ class GameRepositoryImpl(GameRepository):
 
     def save(self, playerDiceMap):
         self.__gameResult = playerDiceMap # dict에 추가
+
+    def checkDiceGameWinner(self):
+        maxDiceNumber = max(self.__gameResult.values()) # dict인 __gameResult의 value 추출
+        # {}의 정보를 player, dice로 받아주고 for loop => 이때 max값과 같은 경우의 player를 반환
+        maxPlayerList = [player for player, dice in self.__gameResult.items() \
+                         if dice == maxDiceNumber]
+        if len(maxPlayerList) > 1:
+            print("무승부입니다!")
+            return
+        # tip if else 형태보다는 if if if 형태가 더 좋습니다.
+        # if
+        #   if 형태의 다단 들여쓰기 형식 아님
+        # 협업이나 깃허브 제출할 때 이렇게 해라
+        print(f"승자: {maxPlayerList[0]}")
--- python/HojoonLee/third/dice_game/game/service/game_service_impl.py
+++ python/HojoonLee/third/dice_game/game/service/game_service_impl.py
@@ -40,10 +40,21 @@ class GameServiceImpl(GameService):
 
         # 승자 발표 >> 무승부 처리 필요
         # hash에 get을 해주면 value값이 반환됨
-        maxDicePlayer = max(playerDiceMap, key=playerDiceMap.get) # max에 key값 넣을 수도 있구나..
-        print(f"승자 {maxDicePlayer}")
+        #maxDicePlayer = max(playerDiceMap, key=playerDiceMap.get) # max에 key값 넣을 수도 있구나..
+        #print(f"승자 {maxDicePlayer}")
 
         # 게임결과 저장을 위함 -> 게임결과 나중에 game entity에 넣기
         self.__gameRepository.save(playerDiceMap)
 
+    def checkDiceGameWinner(self):
+        # Q) 위에 register는 중요한데 여기 check 부분은 굳이 service에서 부터 구현해야하나?
+        # 저장할거면 repo에만 구현하면 되는데..
+        # A) SW를 개발 할 때는 당장 눈앞의 결과도 중요하지만
+        # 비즈니스 지속성을 위해 미래 또한 고려하지 않을 수 없습니다.
+        # 그러므로 '왜 이런 쓸데 없는 행위를 하지?' 라는 관점이 아닌
+        # 추후 이 부분이 확장 될 때 어떤 Domain과 협력할지 보장 할 수 없기 때문에
+        # 아래와 같이 단순히 Repository를 호출하는 경우에도
+        # Service, Repository Layer를 구성합니다.
+        # 한 마디로 확장성 고려 (scailability)
+        self.__gameRepository.checkDiceGameWinner() # 게임 repo의 checkWinner 함수 호출
 
