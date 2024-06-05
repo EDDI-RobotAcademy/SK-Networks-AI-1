09897d0cc34a1ddcc39085ead722b12692ec8274
JUNGUIHEON <jungui4366@gmail.com>
2024-06-05 17:55:44 +0900

    feat: [정의헌] ui폴더 수정 [SKAIPx]

--- ui/JUNGUIHEON/first_1/src/store/index.ts
+++ ui/JUNGUIHEON/first_1/src/store/index.ts
@@ -1,5 +1,4 @@
 import { createStore } from 'vuex'
-import boardModule from "@/board/store/boardModule"
 
 export default createStore({
   state: {
@@ -11,6 +10,5 @@ export default createStore({
   actions: {
   },
   modules: {
-    boardModule
   }
 })
---
+++ ui/JUNGUIHEON/first_1/src/utility/axiosInstance.ts
@@ -0,0 +1,5 @@
+import axios, {AxiosInstance} from "axios";
+
+const djangoAxiosInst: AxiosInstance = axios.create({
+    
+})
