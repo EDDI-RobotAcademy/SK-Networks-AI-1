f9c9a7b894fe46c1151d6b7a1b1e7650250f0114
Gomin <138251577+MinMessi@users.noreply.github.com>
2024-06-05 18:08:13 +0900

    feat [민경원] Django Vue 구성 404에러까지 [SKAIPx]

--- django/KyeongwonMin/manual/manual_proj/manual_proj/urls.py
+++ django/KyeongwonMin/manual/manual_proj/manual_proj/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for first_django project.
+URL configuration for manual_proj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
