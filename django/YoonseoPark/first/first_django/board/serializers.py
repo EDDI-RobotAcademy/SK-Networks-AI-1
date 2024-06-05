2be1735149d58d15e443782f91f8f071a839b719
Yoonseoparkk <patpark97@gmail.com>
2024-06-05 15:14:41 +0900

    update: [박윤서] BoardSerializer 오류 해결 [SKAIPx]

--- django/YoonseoPark/first/first_django/board/serializers.py
+++ django/YoonseoPark/first/first_django/board/serializers.py
@@ -1,6 +1,7 @@
-import serializers
+from rest_framework import serializers
 
 from board.entity.models import Board
+
 
 # 실제 사용할 데이터의 형식이 무엇인지를 알려줍니다
 class BoardSerializer(serializers.ModelSerializer):
