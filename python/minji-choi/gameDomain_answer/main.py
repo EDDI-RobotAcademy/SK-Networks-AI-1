b5311f2bffb4e1e9e5653242c70f96faf6e4480e
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-07 12:41:56 +0900

    chore: [최민지] python 디렉토리 구조 변경 [SKAIPx]

--- python/minji-choi/gameDomain_answer/main.py
+++ python/minji-choi/gameDomain_answer/main.py
@@ -1,6 +1,6 @@
 from Initializer.domain_initializer import DomainInitializer
 from player.service.player_service_impl import PlayerServiceImpl
-from dice.service.dice_service_impl import DiceServiceImpl
+from gameDomain_answer.dice.service.dice_service_impl import DiceServiceImpl
 from game.service.game_service_impl import GameServiceImpl
 
 DomainInitializer.initEachDomain()
