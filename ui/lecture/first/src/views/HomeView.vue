fc80688a66c915a0796291c56c527e09e00ee014
Sanghoon Lee <gcccompil3r@gmail.com>
2024-06-07 17:02:22 +0900

    fix: [이상훈] env 설정 적용 문제 [SKAIPx]

--- ui/lecture/first/src/views/HomeView.vue
+++ ui/lecture/first/src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-  <hello-world />
+  <p>호미다</p>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
   },
 });
 </script>
