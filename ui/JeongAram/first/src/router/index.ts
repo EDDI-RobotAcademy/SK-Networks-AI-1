6a0bad63dd840f5862a419918c0585df8f3d8e6b
Ah-ram <arongj6317@gmail.com>
2024-06-07 17:55:30 +0900

    feat: [정아람] Board App 수정 [SKAPIx]

--- ui/JeongAram/first/src/router/index.ts
+++ ui/JeongAram/first/src/router/index.ts
@@ -10,8 +10,8 @@ const routes: Array<RouteRecordRaw> = [
   },
   // 실제 Javascript 던 Typescript 던 spread 문법을 가지고 있습니다.
   // 내부에 가지고 있는 내용을 그대로 분산시켜서 뒤에 붙이는 작업입니다.
-  // 그러므로...BoardRoutes
- ...BoardRoutes
+  // 그러므로 ...BoardRoutes 는 BoardRoutes 내에 있는 모든 내용을 뒤에 붙입니다.
+  ...BoardRoutes
 ]
 
 const router = createRouter({
