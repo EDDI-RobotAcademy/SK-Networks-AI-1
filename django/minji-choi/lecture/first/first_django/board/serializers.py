c001021eb6fd631a6bd0edd891123a6269175a78
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-10 17:29:48 +0900

    chore: 제거

--- django/minji-choi/lecture/first/first_django/board/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from board.entity.models import Board
-
-
-class BoardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Board
-        fields = ['boardId','title','writer','content','regDate','upDate']
-        read_only_fields = ['regDate','upDate']
--- django/minji-choi/lecture/first/first_django/board/service/board_service.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BoardService(ABC):
-    @abstractmethod
-    def list(self):
-        pass
