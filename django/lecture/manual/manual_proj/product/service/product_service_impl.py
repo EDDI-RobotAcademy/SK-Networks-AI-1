8cca1b8d252dfaeddec4f6f6e078f04d547b8358
Sanghoon Lee <gcccompil3r@gmail.com>
2024-06-12 11:08:20 +0900

    add: [이상훈] 상품 등록 시 이미지 처리 추가 [EXER5-PRAC-2]

--- django/lecture/manual/manual_proj/product/service/product_service_impl.py
+++ django/lecture/manual/manual_proj/product/service/product_service_impl.py
@@ -22,7 +22,8 @@ class ProductServiceImpl(ProductService):
     def list(self):
         return self.__productRepository.list()
 
-    def createProduct(self, productInfo):
-        return self.__productRepository.create(productInfo)
+    def createProduct(self, productName, productPrice, productDescription, productImage):
+        return self.__productRepository.create(
+            productName, productPrice, productDescription, productImage)
 
 
