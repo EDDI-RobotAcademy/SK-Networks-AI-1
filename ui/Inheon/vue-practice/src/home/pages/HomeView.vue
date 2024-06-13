ab9b1185c8d553b69c813740262def635c2a5cb9
Inheon Choi <cih956964@gmail.com>
2024-06-13 17:34:07 +0900

    init: [최인헌] Vue 기존 환경 삭제 후 Board, Product 재구성

---
+++ ui/Inheon/vue-practice/src/home/pages/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>이거시호미다</p>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'HomeView',
+  
+    components: {
+    },
+  });
+  </script>
+  
