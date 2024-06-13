ac858de4cf204dc9e367858194a7ca8d058f2778
jimin9703 <moses0322@naver.com>
2024-06-13 21:11:24 +0900

    feat: [김지민] 금일 수업 코드 [SKAIPx]

---
+++ ui/Jimin/first/src/account/pages/AccountLoginPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-container>
+        
+        <v-img class="mx auto" height="300" :src="require('@/assets/images/fixed/sk_family_ai.png')"/>
+        
+        <div class="login-icons">
+            <v-img class="icon" :src="require('@/assets/images/fixed/loglog.jpg')" height="300" @click="goToKakaoLogin"/>
+        </div>
+    </v-container>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+
+const authenticationModule = 'authenticationModule'
+
+export default{
+    setup() {
+        const store = useStore()
+
+        const goToKakaoLogin = async () => {
+            await store.dispatch("authenticationModule/requestKakaoOauthRedirectionToDjango")
+        }
+
+        return {
+            goToKakaoLogin
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.login_icons {
+    margin-top: 20px;
+}
+
+.icons {
+    cursor: pointer;
+}
+</style>
