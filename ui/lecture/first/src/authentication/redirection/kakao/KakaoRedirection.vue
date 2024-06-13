c24b1b31f64d9757684b36478057290f87cdf424
Sanghoon Lee <gcccompil3r@gmail.com>
2024-06-13 16:39:49 +0900

    feat: [이상훈] Access Token 카카오에 요청 완료 [SKAIPx]

--- ui/lecture/first/src/authentication/redirection/kakao/KakaoRedirection.vue
+++ ui/lecture/first/src/authentication/redirection/kakao/KakaoRedirection.vue
@@ -10,12 +10,15 @@ const accountModule = 'accountModule'
 
 export default {
     methods: {
-        // ...mapActions(authenticationModule, 
-        //     ['requestAccessTokenToDjangoRedirection', 'requestUserInfoToDjango']),
+        ...mapActions(authenticationModule, 
+            ['requestAccessTokenToDjangoRedirection', 'requestUserInfoToDjango']),
         // ...mapActions(accountModule, ['requestEmailDuplicationCheckToDjango']),
         async setRedirectData () {
             const code = this.$route.query.code
             console.log('code:', code)
+
+            await this.requestAccessTokenToDjangoRedirection({ code })
+            // const userInfo = this.requestUserInfoToDjango()
         }
     },
     async created () {
