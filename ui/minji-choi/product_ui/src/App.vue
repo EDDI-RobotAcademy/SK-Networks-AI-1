4cc89326f7036b6c494b89c923a9c5724f7526ca
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-13 10:49:32 +0900

    feat: [최민지] Navigation bar Menu 추가 [SKAIPx]

--- ui/minji-choi/product_ui/src/App.vue
+++ ui/minji-choi/product_ui/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <v-app>
+    <!-- HTML 파트 -->
+    <navigation-menu-bar/>  <!-- 항상 모든 페이지 위에 있도록 -->
     <v-main>
       <router-view/>
     </v-main>
@@ -7,9 +9,12 @@
 </template>
 
 <script lang="ts">
+// 자바스크립트 파트
+import NavigationMenuBar from './navigationMenuBar/NavigationMenuBar.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
+  components: { NavigationMenuBar },
   name: 'App',
 
   data () {
@@ -19,3 +24,4 @@ export default defineComponent({
   },
 })
 </script>
+<!-- 여기 하단에 CSS 작업(style 지정) 파트 -->
