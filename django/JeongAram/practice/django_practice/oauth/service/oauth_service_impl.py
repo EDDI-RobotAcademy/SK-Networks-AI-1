4a7e4bff7b109e7fad85ebe528610e4b05359b6d
Ah-ram <arongj6317@gmail.com>
2024-06-13 15:46:05 +0900

    add: [정아람] 카카오 Oauth 환경 변수 적용 [SKAIPx]

---
+++ django/JeongAram/practice/django_practice/oauth/service/oauth_service_impl.py
@@ -0,0 +1,32 @@
+from oauth.service.oauth_service import OauthService
+
+
+class OauthServiceImpl(OauthService):
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+
+            from django_practice import settings
+            cls.__instance.loginUrl = settings.KAKAO['LOGIN_URL']
+            cls.__instance.clientId = settings.KAKAO['CLIENT_ID']
+            cls.__instance.redirectUri = settings.KAKAO['REDIRECT_URI']
+            cls.__instance.tokenRequestUri = settings.KAKAO['TOKEN_REQUEST_URI']
+            cls.__instance.userinfoRequestUri = settings.KAKAO['USERINFO_REQUEST_URI']
+
+        return cls.__instance
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+
+        return cls.__instance
+
+
+    # https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#request-code
+    def kakaoLoginAddress(self):
+        print("kakaoLoginAddress()")
+        return (f"{self.loginUrl}/oauth/authorize?"
+                f"client_id={self.clientId}&redirect_uri={self.redirectUri}&response_type=code")
