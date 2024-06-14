cabbbd09cf6081cbef084b32197583602ffecfba
Yoonseoparkk <patpark97@gmail.com>
2024-06-14 09:49:30 +0900

    feat: [박윤서] 이메일 중복 체크 요청 [SKAIPx]

--- ui/YoonseoPark/first/src/authentication/store/actions.ts
+++ ui/YoonseoPark/first/src/authentication/store/actions.ts
@@ -5,6 +5,11 @@ import axiosInst from "@/utility/axiosInstance"
 
 export type AuthenticationActions = {
     requestKakaoOauthRedirectionToDjango(): Promise<void>
+    // TypeScript에서 함수 작성 시 파라미터에는 아래와 같이 타입을 명시해야 합니다.
+    // 파라미터 이름: 파라미터 타입 형태로 작성
+    // 함수의 리턴 값은 파라미터를 표현하는 소괄호'(' 에서 시작해서 ')'로 끝난 지점 이후
+    // ':' 을 사용하여 표현
+    // 즉 testFunction(testNumber: number): number
     requestAccessTokenToDjangoRedirection(
         context: ActionContext<AuthenticationState, any>,
         payload: { code: string }): Promise<void>
@@ -49,9 +54,16 @@ const actions: AuthenticationActions = {
                     { access_token: accessToken });
 
             console.log('User Info:', userInfoResponse.data.user_info);
-            return userInfoResponse.data.user_info;
+            const userInfo = userInfoResponse.data.user_info
+
+            return userInfo
         } catch (error) {
             alert('사용자 정보 가져오기 실패!');
+            // JavaScript, TypeScript, python, TypeScript 등등
+            // 최신 스펙을 가진 언어의 경우 세미콜론을 붙일 필요가 없습니다.
+            // 보통 멀티 랭귀지 프로젝트에서는 Poly Glot(폴리 글랏)이란 것을 하게 됨
+            // 세미콜론을 붙이는 습관이 있는 사람도 있고 없는 사람도 있기 때문에 혼선 방지 서포트 역할
+            // 주로 MSA(Micro Service Architecture) 프로젝트를 하면 이런 상황이 비일비재
             throw error;
         }
     }
