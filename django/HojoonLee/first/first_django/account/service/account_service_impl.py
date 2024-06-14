7ec66ae6bba4b6974250ae530f9fc5843eb75b2d
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-06-14 15:48:34 +0900

    feat: [이호준] 닉네임 신청후 db 등록완료 [SKAIPx]

--- django/HojoonLee/first/first_django/account/service/account_service_impl.py
+++ django/HojoonLee/first/first_django/account/service/account_service_impl.py
@@ -1,14 +1,17 @@
+from account.repository.account_repository_impl import AccountRepositoryImpl
 from account.repository.profile_repository_impl import ProfileRepositoryImpl
 from account.service.account_service import AccountService
 
-class AccountServiceImpl(AccountService):
 
+class AccountServiceImpl(AccountService):
     __instance = None
 
     def __new__(cls):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
             cls.__instance.__profileRepository = ProfileRepositoryImpl.getInstance()
+            cls.__instance.__accountRepository = AccountRepositoryImpl.getInstance()
+
         return cls.__instance
 
     @classmethod
@@ -20,4 +23,12 @@ class AccountServiceImpl(AccountService):
 
     def checkEmailDuplication(self, email):
         profile = self.__profileRepository.findByEmail(email)
-        return profile is not None # profile 객체 존재하면 반환 없으면 None
+        return profile is not None
+
+    def checkNicknameDuplication(self, nickname):
+        profile = self.__profileRepository.findByNickname(nickname)
+        return profile is not None
+
+    def registerAccount(self, loginType, roleType, nickname, email):
+        account = self.__accountRepository.create(loginType, roleType)
+        return self.__profileRepository.create(nickname, email, account)
