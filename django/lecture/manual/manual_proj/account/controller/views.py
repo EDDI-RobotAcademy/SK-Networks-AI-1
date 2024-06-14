ab3fa81dda09542dac65ccb57a3bd9abe1170440
Sanghoon Lee <gcccompil3r@gmail.com>
2024-06-14 14:17:15 +0900

    feat: [이상훈] 닉네임 중복 검사 완료 [SKAIPx]

--- django/lecture/manual/manual_proj/account/controller/views.py
+++ django/lecture/manual/manual_proj/account/controller/views.py
@@ -21,3 +21,17 @@ class AccountView(viewsets.ViewSet):
         except Exception as e:
             print("이메일 중복 체크 중 에러 발생:", e)
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def checkNicknameDuplication(self, request):
+        print("checkNicknameDuplication()")
+
+        try:
+            nickname = request.data.get('newNickname')
+            print(f"nickname: {nickname}")
+            isDuplicate = self.accountService.checkNicknameDuplication(nickname)
+
+            return Response({'isDuplicate': isDuplicate, 'message': 'Nickname이 이미 존재' \
+                             if isDuplicate else 'Nickname 사용 가능'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            print("닉네임 중복 체크 중 에러 발생:", e)
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- django/lecture/manual/manual_proj/account/service/account_service.py
+++ django/lecture/manual/manual_proj/account/service/account_service.py
@@ -5,3 +5,7 @@ class AccountService(ABC):
     @abstractmethod
     def checkEmailDuplication(self, email):
         pass
+
+    @abstractmethod
+    def checkNicknameDuplication(self, nickname):
+        pass
