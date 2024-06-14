7f31079d022dd170342ad93f05f8babbbf00939b
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-14 16:21:28 +0900

    feat: [최민지] 이메일 중복체크하여 기존 회원 구별하는 기능 추가(django) [SKAIPx]

--- django/minji-choi/product_pro/manual_proj/account/repository/profile_repository_impl.py
+++ django/minji-choi/product_pro/manual_proj/account/repository/profile_repository_impl.py
@@ -1,5 +1,4 @@
 from account.entity.profile import Profile
-
 from account.repository.profile_repository import ProfileRepository
 
 
@@ -29,3 +28,19 @@ class ProfileRepositoryImpl(ProfileRepository):
         except Exception as e:
             print(f"이메일 중복 검사 중 에러 발생: {e}")
             return None
+
+    def findByNickname(self, nickname):
+        try:
+            profile = Profile.objects.get(nickname=nickname)
+            print(nickname)
+            return profile
+        except Profile.DoesNotExist:
+            print(f"nickname으로 profile 찾을 수 없음: {nickname}")
+            return None
+        except Exception as e:
+            print(f"닉네임 중복 검사 중 에러 발생: {e}")
+            return None
+
+    def create(self, nickname, email, account):
+        profile = Profile.objects.create(nickname=nickname, email=email, account=account)
+        return profile
--- django/minji-choi/product_pro/manual_proj/account/service/account_service.py
+++ django/minji-choi/product_pro/manual_proj/account/service/account_service.py
@@ -4,3 +4,10 @@ class AccountService(ABC):
     @abstractmethod
     def checkEmailDuplication(self,email):
         pass
+    @abstractmethod
+    def checkNicknameDuplication(self, nickname):
+        pass
+    @abstractmethod
+    def registerAccount(self, loginType, roleType, nickname, email):
+        pass
+
