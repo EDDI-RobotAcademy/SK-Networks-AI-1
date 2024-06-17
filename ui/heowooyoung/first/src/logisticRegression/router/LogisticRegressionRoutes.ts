03b08b61afe70e91660450dd6b3e3d39ef57c9aa
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-06-17 14:35:38 +0900

    feat: [허우영] Logistic Regression Vue로 작성 [SKAIPx]

---
+++ ui/heowooyoung/first/src/logisticRegression/router/LogisticRegressionRoutes.ts
@@ -0,0 +1,11 @@
+import LogisticRegressionResultPage from "@/logisticRegression/pages/LogisticRegressionResultPage.vue"
+
+const LogisticRegressionRoutes = [
+    {
+        path: '/logistic-regression-result',
+        name: 'LogisticRegressionResultPage',
+        component: LogisticRegressionResultPage
+    },
+]
+
+export default LogisticRegressionRoutes
