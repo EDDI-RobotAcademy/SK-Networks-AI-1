61a55b7344b0013f472cce9aebc20c26e3bd56ca
ruvi <60618330+RUVIST@users.noreply.github.com>
2024-06-18 16:40:21 +0900

    feat: [최명근] 상품 조회 기능 구현 수업완료 [SKAIPx]

--- django/ChoiMyunggeun/manual/manual_proj/account/urls.py
+++ django/ChoiMyunggeun/manual/manual_proj/account/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('nickname-duplication-check',
          AccountView.as_view({'post': 'checkNicknameDuplication'}),
          name='account-nickname-duplication-check'),
+
 ]
