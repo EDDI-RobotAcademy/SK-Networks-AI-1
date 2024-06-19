103b96a59e4f6b234e70ca2c310c10129425b7ad
Ah-ram <arongj6317@gmail.com>
2024-06-19 20:23:47 +0900

    feat: [정아람] fastapi 수업내용 [SKAIPx]

---
+++ fastapiAI/JeongAram/fastapi_demo/polynomialRegression/service/polynomial_regression_service_impl.py
@@ -0,0 +1,26 @@
+from polynomialRegression.repository.polynomial_regression_repository_impl import PolynomialRegressionRepositoryImpl
+from polynomialRegression.service.polynomial_regression_service import  PolynomialRegressionService
+
+import numpy as np
+
+class PolynomialRegressionServiceImpl(PolynomialRegressionService):
+
+    def __init__(self):
+        self.polynomialRegressionRepository = PolynomialRegressionRepositoryImpl()
+
+    async def generateSampleData(self):
+        np.random.seed(0)
+        X = 2 - 3 * np.random.normal(0, 1, 100)
+        print(f"X: {X}")
+        y = X - 2 * (X ** 2) + np.random.normal(-3, 3, 100)
+        print(f"y: {y}")
+        X = X[:, np.newaxis] # [1, 100] 1행 100열 이였던 것 -> [100, 1]
+        print(f"after X: {X}")
+
+        # 행렬의 연산 조건 (n X m) . (k x p)
+        # 기본적으로 m과 k가 일치해야 연산할 수 있으므로 차원 이동시킨 것
+        # 물론 pseudo 행렬은 저거 안 맞아도 할 수 있긴 하지만 이건 너무 넘어감
+        return X, y
+    async def createSamplePolynomialRegression(self):
+        X, y = await self.generateSampleData()
+        return self.polynomialRegressionRepository.regressionAnalysis(X, y)
