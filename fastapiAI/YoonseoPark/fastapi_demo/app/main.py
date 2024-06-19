910400ef9b08d48aa5eb7b5301cc8b40fb3ce497
Yoonseoparkk <patpark97@gmail.com>
2024-06-19 10:36:23 +0900

    feat: [박윤서] DDD 기반 다항 회귀 분석 [SKAIPx]

--- fastapiAI/YoonseoPark/fastapi_demo/app/main.py
+++ fastapiAI/YoonseoPark/fastapi_demo/app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from logistic_regression.controller.logistic_regression_controller import logisticRegressionRouter
+from polynomialRegression.controller.polynomial_regression_controller import polynomialRegressionRouter
 from train_test_evaluation.controller.train_test_evaulation_controller import trainTestEvaluationRouter
 
 app = FastAPI()
@@ -57,6 +58,7 @@ def read_item(item_id: int, q: str = None):
 
 app.include_router(logisticRegressionRouter)
 app.include_router(trainTestEvaluationRouter)
+app.include_router(polynomialRegressionRouter)
 
 load_dotenv()
 
