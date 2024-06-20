1358a66ab85574fc839f72daabc41ec8d772cb68
Sanghoon Lee <gcccompil3r@gmail.com>
2024-06-20 09:50:28 +0900

    feat: [이상훈] Exponential Regression 요청 처리 [SKAIPx]

--- fastapiAI/lecture/fastapi_demo/exponential_regression/controller/exponential_regression_controller.py
+++ fastapiAI/lecture/fastapi_demo/exponential_regression/controller/exponential_regression_controller.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 
-from exponential_regression.service.exponential_service_impl import ExponentialRegressionServiceImpl
+from exponential_regression.service.exponential_regression_service_impl import ExponentialRegressionServiceImpl
 
 exponentialRegressionRouter = APIRouter()
 
