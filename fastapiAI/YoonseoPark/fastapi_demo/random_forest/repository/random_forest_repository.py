d04930104aaeb66d2e0da4f509b02f858af0e911
Yoonseoparkk <patpark97@gmail.com>
2024-06-21 10:02:14 +0900

    feat: [박윤서] 항공 예약 정보 피쳐 설정 [SKAIPx]

---
+++ fastapiAI/YoonseoPark/fastapi_demo/random_forest/repository/random_forest_repository.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class RandomForestRepository(ABC):
+    @abstractmethod
+    def evaluate(self):
+        pass
+
+    @abstractmethod
+    def flightCategoricalVariableEncoding(self, dataFrame):
+        pass
