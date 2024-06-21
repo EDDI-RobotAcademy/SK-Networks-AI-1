43fe24927fb98217e104960d59cf5b8c4a0f7487
lleehs <lhs3789@gmail.com>
2024-06-21 15:41:59 +0900

    feat: [이현석] 2024-06-20 cart만들기 [SKAIPx]
    feat: [이현석] 2024-06-20 cart만들기 [SKAIPx]

---
+++ django/LeeHyunSeok/manual/manual_proj/cart/repository/cart_item_repository_impl.py
@@ -0,0 +1,31 @@
+from cart.entity.cart import Cart
+from cart.entity.cart_item import CartItem
+from cart.repository.cart_item_repository import CartItemRepository
+
+
+class CartItemRepositoryImpl(CartItemRepository):
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+
+        return cls.__instance
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+
+        return cls.__instance
+
+    def register(self, cartData, cart, product):
+        productPrice = cartData.get('productPrice')
+        quantity = cartData.get('quantity')
+
+        CartItem.objects.create(
+            cart=cart,
+            product=product,
+            quantity=quantity,
+            price=productPrice
+        )
