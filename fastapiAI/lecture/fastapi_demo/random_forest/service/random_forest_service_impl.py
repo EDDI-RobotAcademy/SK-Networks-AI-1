66f9d0ee027e6c34d8c2ae5a780073079f6c881d
Sanghoon Lee <gcccompil3r@gmail.com>
2024-06-21 09:49:40 +0900

    feat: [이상훈] 항공 예약 정보 피쳐 설정 [SKAIPx]

--- fastapiAI/lecture/fastapi_demo/random_forest/service/random_forest_service_impl.py
+++ fastapiAI/lecture/fastapi_demo/random_forest/service/random_forest_service_impl.py
@@ -1,13 +1,45 @@
+import os
+
 from random_forest.repository.random_forest_repository_impl import RandomForestRepositoryImpl
 from random_forest.service.random_forest_service import RandomForestService
+
+import pandas as pd
 
 
 class RandomForestServiceImpl(RandomForestService):
     def __init__(self):
         self.__randomForestRepository = RandomForestRepositoryImpl()
 
+    def readCsv(self):
+        currentDirectory = os.getcwd()
+        # print(f"currentDirectory: {currentDirectory}")
+
+        filePath = os.path.join(currentDirectory, '..', 'assets', 'customer_booking.csv')
+        # print(f"filePath: {filePath}")
+
+        dataFrame = pd.read_csv(filePath, encoding='latin1')
+        # print(f"dataFrame: {dataFrame}")
+        return dataFrame
+
+    # 특징 타겟 변수를 정의
+    def featureTargetVariableDefinition(self, dataEncoded):
+        print("featureTargetVariableDefinition()")
+
+        X = dataEncoded.drop('booking_complete', axis=1)
+        y = dataEncoded['booking_complete']
+
+        return X, y
+
     def randomForestAnalysis(self):
         print("randomForestAnalysis()")
+
+        dataFrame = self.readCsv()
+        dataEncoded, labelEncoders = (
+            self.__randomForestRepository.flightCategoricalVariableEncoding(dataFrame))
+
+        X, y = self.featureTargetVariableDefinition(dataEncoded)
+        # print(f"X: {X}")
+        # print(f"y: {y}")
 
         self.__randomForestRepository.evaluate()
 
