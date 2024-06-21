118f55a8a2837a394839d077b20bac8694962520
JUNGUIHEON <jungui4366@gmail.com>
2024-06-21 12:14:22 +0900

    feat: [정의헌] Confusion Matrix 출력 완료 [SKAIPx]

--- ui/JUNGUIHEON/first/src/randomForest/pages/RandomForestResultPage.vue
+++ ui/JUNGUIHEON/first/src/randomForest/pages/RandomForestResultPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container>
+    <v-container v-if="confusionMatrixInfoBeforeSmote && confusionMatrixInfoAfterSmote">
         <v-row>
             <v-col cols="6">
                 <v-card>
@@ -7,8 +7,8 @@
                         Confusion Matrix before SMOTE
                     </v-card-title>
                     <v-card-text>
-                        <!-- <confusion-matrix 
-                        : confusionMatrix="confusionMatrixInfoBeforeSmote.confusion_matrix"/>-->
+                        <confusion-matrix 
+                        :confusionMatrix="confusionMatrixInfoBeforeSmote.confusion_matrix"/>
                     </v-card-text>
                 </v-card>
             </v-col>
@@ -19,17 +19,22 @@
                         Confusion Matrix after SMOTE
                     </v-card-title>
                     <v-card-text>
-                        <!-- <confusion-matrix 
-                        : confusionMatrix="confusionMatrixInfoBeforeSmote.confusion_matrix"/>-->
+                        <confusion-matrix 
+                        :confusionMatrix="confusionMatrixInfoAfterSmote.confusion_matrix"/>
                     </v-card-text>
                 </v-card>
             </v-col>
-    </v-row>
+        </v-row>
     </v-container>
 </template>
 
 <script>
+import ConfusionMatrix from '@/randomForest/components/ConfusionMatrix.vue'
+
 export default {
+    components: {
+        ConfusionMatrix,
+    },
     data () {
         return {
             confusionMatrixInfoBeforeSmote: null,
@@ -50,11 +55,10 @@ export default {
             const response = await fetch('http://localhost:33333/random-forest')
             const data = await response.json()
 
-            console.log('data: ', data)
+            console.log('data:', data)
 
             this.confusionMatrixInfoBeforeSmote = data.confusion_matrix_info_before_smote
             this.confusionMatrixInfoAfterSmote = data.confusion_matrix_info_after_smote
-
         }
     }
 }
