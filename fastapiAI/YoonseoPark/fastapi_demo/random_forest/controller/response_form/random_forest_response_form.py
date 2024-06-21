1fb4ccdd86c3338c8d831a9f1ac2267a38b5b442
Yoonseoparkk <patpark97@gmail.com>
2024-06-21 14:50:20 +0900

    add: [박윤서] 데이터 타입 관련 오류 해결 [SKAIPx]

--- fastapiAI/YoonseoPark/fastapi_demo/random_forest/controller/response_form/random_forest_response_form.py
+++ fastapiAI/YoonseoPark/fastapi_demo/random_forest/controller/response_form/random_forest_response_form.py
@@ -1,10 +1,11 @@
 class RandomForestResponseForm:
     @staticmethod
-    def createForm(confusionMatrix, smoteConfusionMatrix, y_test, y_pred, y_pred_after_smote, data):
+    def createForm(confusionMatrix, smoteConfusionMatrix,
+                   y_test, y_pred, y_pred_after_smote, data):
 
         # 공통 정보
         common_info = {
-            "passenger_count": data[['num_passengers', 'booking_complete']].to_dict(orient='records'),
+            "passengers_count": data[['num_passengers', 'booking_complete']].to_dict(orient='records'),
             "purchase_lead": data[['purchase_lead', 'booking_complete']].to_dict(orient='records'),
             "length_of_stay": data[['length_of_stay', 'booking_complete']].to_dict(orient='records'),
             "extra_baggage": data[['wants_extra_baggage', 'booking_complete']].to_dict(orient='records'),
@@ -14,12 +15,13 @@ class RandomForestResponseForm:
 
         y_test_list = y_test.tolist()
 
-        # 혼돈 행렬 구성
+        # 혼동 행렬 구성
         confusionMatrixBeforeSmote = {
             'confusion_matrix': confusionMatrix.tolist(),
             'y_test': y_test_list,
             'y_pred': y_pred.tolist()
         }
+
         confusionMatrixAfterSmote = {
             'confusion_matrix': smoteConfusionMatrix.tolist(),
             'y_test': y_test_list,
