f7de3d376437e477bf6105b6230f882ce899a418
0H00NI <yhoon3002@gmail.com>
2024-06-24 15:21:43 +0900

    feat: [임영훈] Vue Post 작성 완료 [SKAIPx]

---
+++ ui/LimYounghoon/assignment/src/post/store/actions.ts
@@ -0,0 +1,53 @@
+import { ActionContext } from "vuex";
+import { Post, PostState } from "./states";
+import { AxiosResponse } from "axios";
+import axiosInst from "@/utility/axiosInstance";
+import { REQUEST_POST_LIST_TO_FASTAPI } from "./mutation-types";
+
+export type PostActions = {
+    requestPostListToFastapi(
+        context: ActionContext<PostState, any>
+    ): Promise<void>;
+    requestCreatePostToFastapi(
+        context: ActionContext<PostState, unknown>,
+        payload: { title: string; content: string }
+    ): Promise<number>;
+};
+
+const actions: PostActions = {
+    async requestPostListToFastapi(
+        context: ActionContext<PostState, any>
+    ): Promise<void> {
+        try {
+            const res: AxiosResponse<any, any> =
+                await axiosInst.fastapiAxiosInst.get("/post/list");
+            const data: Post[] = res.data;
+            context.commit(REQUEST_POST_LIST_TO_FASTAPI, data);
+        } catch (error) {
+            console.error("requestPostListToFastapi() 중 에러 발생", error);
+            throw error;
+        }
+    },
+
+    async requestCreatePostToFastapi(
+        context: ActionContext<PostState, unknown>,
+        payload: { title: string; content: string }
+    ): Promise<number> {
+        const { title, content } = payload;
+
+        try {
+            const res = await axiosInst.fastapiAxiosInst.post("/post/create", {
+                title,
+                content,
+            });
+
+            console.log("res:", res.data);
+            return res.data.id;
+        } catch (error) {
+            console.error("requestCreatePostToFastapi() 중 에러 발생:", error);
+            throw error;
+        }
+    },
+};
+
+export default actions;
