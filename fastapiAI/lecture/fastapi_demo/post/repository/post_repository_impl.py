63b8e1a26e1066fd82fe8efcb28f2021f14fd821
Sanghoon Lee <gcccompil3r@gmail.com>
2024-06-24 12:25:16 +0900

    feat: [이상훈] 익명 포스트 조회 [SKAIPx]

--- fastapiAI/lecture/fastapi_demo/post/repository/post_repository_impl.py
+++ fastapiAI/lecture/fastapi_demo/post/repository/post_repository_impl.py
@@ -15,6 +15,7 @@ class PostRepositoryImpl(PostRepository):
 
         async with self.dbPool.acquire() as connection:
             async with connection.cursor() as cur:
+                # post에 있는 모든 id, title, content를 찾아와서 postList를 만들어 반환
                 await cur.execute("select id, title, content from post")
                 result = await cur.fetchall()
                 postList = [Post(id=row[0], title=row[1], content=row[2]) for row in result]
