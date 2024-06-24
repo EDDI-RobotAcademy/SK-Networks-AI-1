6acacbfed63f20678c20dcd42471dddc0e6b650a
0H00NI <yhoon3002@gmail.com>
2024-06-24 10:30:10 +0900

    feat: [임영훈] vue d3plot [SKAIPx]

--- ui/LimYounghoon/assignment/src/d3plot/router/D3PlotRoutes.ts
+++ ui/LimYounghoon/assignment/src/d3plot/router/D3PlotRoutes.ts
@@ -1,17 +1,17 @@
-import BarChart from "@/d3plot/components/barChart/BarChart.vue";
-import PieChart from "@/d3plot/components/pieChart/PieChart.vue";
+import BarChart from '@/d3plot/components/barChart/BarChart.vue'
+import PieChart from '@/d3plot/components/pieChart/PieChart.vue'
 
 const D3PlotRoutes = [
     {
-        path: "/d3plot/bar-chart",
-        name: "BarChart",
+        path: '/d3plot/bar-chart',
+        name: 'BarChart',
         component: BarChart,
     },
     {
-        path: "/d3plot/pie-chart",
-        name: "PieChart",
+        path: '/d3plot/pie-chart',
+        name: 'PieChart',
         component: PieChart,
     },
-];
+]
 
-export default D3PlotRoutes;
+export default D3PlotRoutes
