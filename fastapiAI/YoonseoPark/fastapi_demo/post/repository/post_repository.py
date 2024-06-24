85946635f3d40a7b460c0b73974b82b8f3fdc828
Yoonseoparkk <patpark97@gmail.com>
2024-06-24 11:21:35 +0900

    feat: [박윤서] sql 파일 읽어서 DB 자동 추가 [SKAIPx]

---
+++ fastapiAI/YoonseoPark/fastapi_demo/post/repository/post_repository.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from post.entity.models import Post
+
+
+class PostRepository(ABC):
+    @abstractmethod
+    def list(self) -> List[Post]:
+        pass
