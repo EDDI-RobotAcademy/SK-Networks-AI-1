abe44967ece825bdd77069a39dd75b2b0e76bc20
Yoonseoparkk <patpark97@gmail.com>
2024-06-24 16:41:54 +0900

    feat: [박윤서] FastAPI 기반 Post 처리 완료 [SKAIPx]

--- fastapiAI/YoonseoPark/fastapi_demo/post/entity/models.py
+++ fastapiAI/YoonseoPark/fastapi_demo/post/entity/models.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 
-
 class Post(BaseModel):
     id: int = None
     title: str
--- fastapiAI/YoonseoPark/fastapi_demo/post/repository/post_repository.py
+++ fastapiAI/YoonseoPark/fastapi_demo/post/repository/post_repository.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional
 
 from post.entity.models import Post
 
@@ -7,4 +7,12 @@ from post.entity.models import Post
 class PostRepository(ABC):
     @abstractmethod
     def list(self) -> List[Post]:
+        pass
+
+    @abstractmethod
+    def create(self, post: Post) -> int:
+        pass
+
+    @abstractmethod
+    def findById(self, postId: int) -> Optional[Post]:
         pass
