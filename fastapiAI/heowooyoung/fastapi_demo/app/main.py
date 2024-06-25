8ac983dbe8fbd7773aa4b2e4d2dd9db2ea5ccab7
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-06-25 17:51:27 +0900

    feat: [허우영] Non AWS 환경에서 서버 구성하기 [SKAIPx]

--- fastapiAI/heowooyoung/fastapi_demo/app/main.py
+++ fastapiAI/heowooyoung/fastapi_demo/app/main.py
@@ -106,4 +106,4 @@ app.add_middleware(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=33333)
+    uvicorn.run(app, host="192.168.0.42", port=33333)
--- ui/heowooyoung/first/src/store/index.ts
+++ ui/heowooyoung/first/src/store/index.ts
@@ -5,7 +5,7 @@ import productModule from "@/product/store/productModule"
 import authenticationModule from "@/authentication/store/authenticationModule"
 import accountModule from '@/account/store/accountModule'
 import cartModule from "@/cart/store/cartModule"
-import postModule from "@/post/store/postModule";
+import postModule from "@/post/store/postModule"
 
 export default createStore({
   state: {
