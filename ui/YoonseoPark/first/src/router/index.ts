2eeffa6185d285e7deb3356330b974e821042268
Yoonseoparkk <patpark97@gmail.com>
2024-06-26 12:22:29 +0900

    feat: [박윤서] 외부 입력을 통한 예측 준비 [SKAIPx]

--- ui/YoonseoPark/first/src/router/index.ts
+++ ui/YoonseoPark/first/src/router/index.ts
@@ -13,6 +13,7 @@ import ExponentialRegressionRoutes from '@/exponentialRegression/router/Exponent
 import RandomForestRoutes from '@/randomForest/router/RandomForestRoutes'
 import PostRoutes from '@/post/router/PostRoutes'
 import KmeansRoutes from '@/kmeans/router/KmeansRoutes'
+import TensorFlowIrisTestRoutes from '@/tfIris/router/TensorFlowIrisTestRoutes'
 
 const routes: Array<RouteRecordRaw> = [
   ...HomeRoutes,
@@ -31,6 +32,7 @@ const routes: Array<RouteRecordRaw> = [
   ...RandomForestRoutes,
   ...PostRoutes,
   ...KmeansRoutes,
+  ...TensorFlowIrisTestRoutes,
 ]
 
 const router = createRouter({
