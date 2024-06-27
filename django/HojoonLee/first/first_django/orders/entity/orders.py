575283878ae5102a80d7efcc6ef952d2e769f854
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-06-27 17:20:22 +0900

    feat: [이호준] Django Order domain 구현 [SKAIPx]

---
+++ django/HojoonLee/first/first_django/orders/entity/orders.py
@@ -0,0 +1,19 @@
+from django.utils import timezone
+from orders.entity.orders_status import OrdersStatus
+from django.db import models
+
+from account.entity.account import Account
+
+
+class Orders(models.Model):
+    id = models.AutoField(primary_key=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='orders')
+    status = models.CharField(max_length=10, choices=OrdersStatus.choices, default=OrdersStatus.PENDING)
+    created_data = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Orders {self.id} by {self.account}"
+
+    class Meta:
+        db_table = 'orders'
+        app_label = 'orders'
