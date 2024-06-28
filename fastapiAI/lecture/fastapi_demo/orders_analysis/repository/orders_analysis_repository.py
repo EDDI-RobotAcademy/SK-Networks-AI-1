4da0d771ee6c1a7bba9a23c8aac92be3411ab910
Sanghoon Lee <gcccompil3r@gmail.com>
2024-06-28 12:27:25 +0900

    feat: [이상훈] View Count에 따른 quantity 구매 수량 예측 [SKAIPx]

--- fastapiAI/lecture/fastapi_demo/orders_analysis/repository/orders_analysis_repository.py
+++ fastapiAI/lecture/fastapi_demo/orders_analysis/repository/orders_analysis_repository.py
@@ -10,3 +10,19 @@ class OrdersAnalysisRepository(ABC):
     @abstractmethod
     def splitTrainTestData(self, X_scaled, y):
         pass
+
+    @abstractmethod
+    def createModel(self):
+        pass
+
+    @abstractmethod
+    def fitModel(self, model, X_train, y_train):
+        pass
+
+    @abstractmethod
+    def transformFromScaler(self, scaler, X_pred):
+        pass
+
+    @abstractmethod
+    def predictFromModel(self, ordersModel, X_pred_scaled):
+        pass
