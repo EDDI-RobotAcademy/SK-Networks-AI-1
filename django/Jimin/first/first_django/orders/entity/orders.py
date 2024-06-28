9c670abbb130c7a35729c167b2397fab1aed454e
jimin9703 <moses0322@naver.com>
2024-06-28 15:37:31 +0900

    feat: [김지민] 제거도 [sk]

---
+++ django/Jimin/first/first_django/orders/entity/orders.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from account.entity.account import Account
+from orders.entity.orders_status import OrderStatus
+from django.utils import timezone
+
+
+class Orders(models.Model):
+    id = models.AutoField(primary_key=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='orders')
+    status = models.CharField(max_length=10, choices=OrderStatus.choices, default=OrderStatus.PENDING)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Orders {self.id} by {self.account}"
+
+    class Meta:
+        db_table = 'orders'
+        app_label = 'orders'
