df34b219c8be3c73a912fac8bcdaa2081dd9cd8b
Ah-ram <arongj6317@gmail.com>
2024-06-28 20:14:37 +0900

    add: [정아람] 코드 주석 추가 [SKAIPx]

--- fastapiAI/JeongAram/fastapi_demo/orders_analysis/service/orders_analysis_service_impl.py
+++ fastapiAI/JeongAram/fastapi_demo/orders_analysis/service/orders_analysis_service_impl.py
@@ -15,9 +15,10 @@ class OrdersAnalysisServiceImpl(OrdersAnalysisService):
         self.__ordersAnalysisRepository = OrdersAnalysisRepositoryImpl()
 
     async def readExcel(self):
-        currentDirectory = os.getcwd()
+        currentDirectory = os.getcwd() # 코드에서 실행하고 있는 디렉토리
         print(f"currentDirectory: {currentDirectory}")
 
+        # 협업할 때는 상대경로 사용 (" " 사이에 / 사용 금지 -> // 이런 형태가 됨)
         filePath = os.path.join(
             currentDirectory, "..", "assets", "orders_data_after_drop_duplication.xlsx"
         )
@@ -32,7 +33,7 @@ class OrdersAnalysisServiceImpl(OrdersAnalysisService):
         dataFrame = await self.readExcel()
         X_scaled, y, scaler = await self.__ordersAnalysisRepository.prepareViewCountVsQuantity(dataFrame)
 
-        joblib.dump(scaler, "ordersModelScaler.pkl")
+        joblib.dump(scaler, "ordersModelScaler.pkl") # joblib: 저장하고자 하는 값 # pkl(피클): 데이터 저장할 때 쓰임
 
         X_train, X_test, y_train, y_test = \
             await self.__ordersAnalysisRepository.splitTrainTestData(X_scaled, y)
@@ -47,10 +48,10 @@ class OrdersAnalysisServiceImpl(OrdersAnalysisService):
 
             model = await self.__ordersAnalysisRepository.createModel()
             print("is it pass createModel()")
-            await self.__ordersAnalysisRepository.fitModel(model, X_train, y_train)
+            await self.__ordersAnalysisRepository.fitModel(model, X_train, y_train) # 훈련된 모델
             print("is it pass fitModel()")
-            model.save(f"ordersModel_{index + 1}.h5")
-            modelList.append(model)
+            model.save(f"ordersModel_{index + 1}.h5") # 앞에 경로가 없으면 os.cwd의 경로와 같음
+            modelList.append(model) # 나중에 가져다 쓸 수 있도록 리스트로 저장
 
         return f"Trained {numberOfModels} models 성공!!"
 
@@ -66,6 +67,7 @@ class OrdersAnalysisServiceImpl(OrdersAnalysisService):
             ordersModel = tf.keras.models.load_model(f"ordersModel_{index}.h5")
             print(f"after load model")
             X_pred = np.array([[viewCount]])
+            # transform을 해주기 위해 2차원으로 [[]] 해줌
             X_pred_scaled = await self.__ordersAnalysisRepository.transformFromScaler(scaler, X_pred)
 
             ordersPredict = await self.__ordersAnalysisRepository.predictFromModel(ordersModel, X_pred_scaled)
