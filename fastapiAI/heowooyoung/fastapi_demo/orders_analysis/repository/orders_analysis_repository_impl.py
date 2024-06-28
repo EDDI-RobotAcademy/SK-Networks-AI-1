6ef2027752d34f537a2fb60d1089b9b6457d8897
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-06-28 15:19:07 +0900

    fix: [허우영] 로그인 상태에서 새로고침 하면 로그인이 풀리는 오류 해결 [SKAIPx]

--- fastapiAI/heowooyoung/fastapi_demo/orders_analysis/repository/orders_analysis_repository_impl.py
+++ fastapiAI/heowooyoung/fastapi_demo/orders_analysis/repository/orders_analysis_repository_impl.py
@@ -17,6 +17,7 @@ class OrdersAnalysisRepositoryImpl(OrdersAnalysisRepository):
 
         return X_scaled, y, scaler
 
+
     async def splitTrainTestData(self, X_scaled, y):
         X_train, X_test, y_train, y_test = train_test_split(
             X_scaled, y, test_size=0.2, random_state=42
