db83bbd997dfda9ca4d7d07634b549d0aa6d269e
Sanghoon Lee <gcccompil3r@gmail.com>
2024-07-01 10:49:14 +0900

    feat: [이상훈] Gradient 계산이 안되는 오류가 발생하고 있음 [SKAIPx]

--- fastapiAI/lecture/fastapi_demo/gradient_descent/repository/gradient_descent_repository.py
+++ fastapiAI/lecture/fastapi_demo/gradient_descent/repository/gradient_descent_repository.py
@@ -5,4 +5,11 @@ class GradientDescentRepository(ABC):
     @abstractmethod
     def createTrainData(self):
         pass
-    
+
+    @abstractmethod
+    def selectLinearRegressionModel(self):
+        pass
+
+    @abstractmethod
+    def trainModel(self, selectedModel, X, y):
+        pass
