e7ab776867f6697b231ef304e211f01c6f91fa98
Sanghoon Lee <gcccompil3r@gmail.com>
2024-07-02 09:34:30 +0900

    feat: [이상훈] 와인 데이터 로딩 [SKAIPx]

---
+++ fastapiAI/lecture/fastapi_demo/decision_tree/repository/decision_tree_repository.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+class DecisionTreeRepository(ABC):
+    @abstractmethod
+    def loadWineInfo(self):
+        pass
--- fastapiAI/lecture/fastapi_demo/decision_tree/service/decision_tree_service_impl.py
+++ fastapiAI/lecture/fastapi_demo/decision_tree/service/decision_tree_service_impl.py
@@ -1,7 +1,14 @@
+from decision_tree.repository.decision_tree_repository_impl import DecisionTreeRepositoryImpl
 from decision_tree.service.decision_tree_service import DecisionTreeService
 
 
 class DecisionTreeServiceImpl(DecisionTreeService):
 
+    def __init__(self):
+        self.decisionTreeRepository = DecisionTreeRepositoryImpl()
+
     def decisionTreeTrain(self):
         print("service -> decisionTreeTrain()")
+
+        wineInfo = self.decisionTreeRepository.loadWineInfo()
+        print(f"wine feature names: {wineInfo.feature_names}")
