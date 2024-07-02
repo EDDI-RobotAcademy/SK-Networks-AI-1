f048024da630a8e74637a3b8c2c567b0a0d1a8a9
Ah-ram <arongj6317@gmail.com>
2024-07-02 14:53:32 +0900

    feat: [정아람] TensorFlow Decision Tree [SKAIPx]

--- fastapiAI/JeongAram/fastapi_demo/app/main.py
+++ fastapiAI/JeongAram/fastapi_demo/app/main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from async_db.database import getMySqlPool, createTableIfNeccessary
+from decision_tree.controller.decision_tree_controller import decisionTreeRouter
 from exponential_regression.controller.exponential_regression_controller import exponentialRegressionRouter
 from gradient_descent.controller.gradient_descent_controller import gradientDescentRouter
 from kmeans.controller.kmeans_controller import kmeansRouter
@@ -111,6 +112,7 @@ app.include_router(kmeansRouter)
 app.include_router(tfIrisRouter)
 app.include_router(ordersAnalysisRouter)
 app.include_router(gradientDescentRouter)
+app.include_router(decisionTreeRouter)
 # 여기까지 해야 router 연결됨
 
 # env 관련 설정
