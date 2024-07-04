39f75ec5f84b76053aea5fa63f3cc3f00262b935
Ah-ram <arongj6317@gmail.com>
2024-07-04 21:45:40 +0900

    feat: [정아람] Kafka [SKAIPx]

---
+++ ui/JeongAram/first/src/kafka/store/mutations.ts
@@ -0,0 +1,17 @@
+import { MutationTree } from "vuex";
+import { KafkaTestState } from "./states";
+import {
+    REQUEST_KAFKA_TEST_DATA_TO_FASTAPI
+} from "./mutation-types";
+
+export interface KafkaTestMutations extends MutationTree<KafkaTestState> {
+    [REQUEST_KAFKA_TEST_DATA_TO_FASTAPI] (state: KafkaTestState, isRequest: boolean): void
+}
+
+const mutations: MutationTree<KafkaTestState> = {
+    [REQUEST_KAFKA_TEST_DATA_TO_FASTAPI] (state: KafkaTestState, isRequest: boolean): void {
+        state.isRequest = isRequest
+    },
+}
+
+export default mutations as KafkaTestMutations
