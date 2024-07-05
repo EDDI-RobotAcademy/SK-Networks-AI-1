07e402e8b325351911f28542f4bdb479d85e177e
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-07-05 12:05:15 +0900

    feat: [허우영] CNN 이미지 분류 [SKAIPx]

---
+++ fastapiAI/heowooyoung/fastapi_demo/convolution_neural_network/controller/cnn_controller.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from starlette.responses import JSONResponse
+
+from convolution_neural_network.service.cnn_service_impl import ConvolutionNeuralNetworkServiceImpl
+
+convolutionNeuralNetworkRouter = APIRouter()
+
+async def injectConvolutionNeuralNetworkService() -> ConvolutionNeuralNetworkServiceImpl:
+    return ConvolutionNeuralNetworkServiceImpl()
+
+
+@convolutionNeuralNetworkRouter.post("/cnn-train")
+async def cnnBasedImageTrain(convolutionNeuralNetworkService: ConvolutionNeuralNetworkServiceImpl =
+                             Depends(injectConvolutionNeuralNetworkService)):
+
+    print(f"controller -> cnnBasedImageTrain()")
+
+    convolutionNeuralNetworkService.imageTrain()
+
+    return JSONResponse(content={"status": "모델 훈련 및 저장이 완료되었습니다!"})
