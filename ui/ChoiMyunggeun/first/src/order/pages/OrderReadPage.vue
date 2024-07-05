377b879c875b54e3f8ea4574274903cf47d2b462
ruvi <60618330+RUVIST@users.noreply.github.com>
2024-07-05 14:37:06 +0900

    feat: [최명근] Order 페이지 구성 [SKAIPx]

--- ui/ChoiMyunggeun/first/src/order/pages/OrderReadPage.vue
+++ ui/ChoiMyunggeun/first/src/order/pages/OrderReadPage.vue
@@ -68,36 +68,21 @@ export default {
         }
     },
     methods: {
-        async fetchOrder() {
-            ...mapActions("orderModule", ["requestReadOrderToDjango"]),
-            async fetchOrderData() {
+        ...mapActions("orderModule", ["requestReadOrderToDjango"]),
+        async fetchOrderData() {
                 const orderId = this.orderId
                 console.log('OrderReadPage orderId :', orderId)
                 
                 try {
                     const response = await this.this.requestReadOrderToDjango({ orderId })
+                    this.order = response
+                    console.log('ordersItemInfo:', this.order)
                 } catch (error) {
                     console.error('주문 내역 확인 중 에러:',error)
                 }
-            },
-            // const orderId = this.$route.params.orderId;
-            // 여기에서 API 호출 또는 Vuex 액션을 통해 주문 데이터를 가져옵니다.
-            // 예시: const response = await this.$store.dispatch('fetchOrder', orderId);
-            // this.order = response;
-            // 여기서는 더미 데이터를 사용합니다.
-            this.order = {
-                orderId: orderId,
-                items: [
-                    { productId: 1, productName: "Product 1", productPrice: 100, quantity: 2 },
-                    { productId: 2, productName: "Product 2", productPrice: 200, quantity: 1 },
-                ]
-            };
         },
-        placeOrder() {
-            // 최종 주문 처리 로직
-            alert("Order has been placed successfully!");
-            // 주문 후 장바구니 초기화 또는 다른 로직 추가
-            this.$router.push({ name: 'HomePage' }); // Assuming you want to redirect to HomePage after order
+        goToBack () {
+            this.$router.push({ name: 'HomeView' })
         }
     },
     created() {
