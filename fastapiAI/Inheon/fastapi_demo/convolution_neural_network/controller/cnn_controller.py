cb813610766e1c5a379354bd77ed665244b9daa2
Inheon Choi <cih956964@gmail.com>
2024-07-09 09:39:45 +0900

    feat: [최인헌] 0709 fastapi 수업 [SKAIPx]

--- fastapiAI/Inheon/fastapi_demo/convolution_neural_network/controller/cnn_controller.py
+++ fastapiAI/Inheon/fastapi_demo/convolution_neural_network/controller/cnn_controller.py
@@ -17,16 +17,17 @@ async def cnnBasedImageTrain(convolutionNeuralNetworkService: ConvolutionNeuralN
 
     convolutionNeuralNetworkService.imageTrain()
 
-    return JSONResponse(content={'status': '모델 훈련 및 저장이 완료되었습니다.'})
+    return JSONResponse(content={"status": "모델 훈련 및 저장이 완료되었습니다!"})
 
-@convolutionNeuralNetworkRouter.post('/cnn-predict')
-async def cnnBasedImagePredict(file: UploadFile=File(...),
+@convolutionNeuralNetworkRouter.post("/cnn-predict")
+async def cnnBasedImagePredict(file: UploadFile = File(...),
                                convolutionNeuralNetworkService: ConvolutionNeuralNetworkServiceImpl =
-                               Depends(injectConvolutionNeuralNetworkService())):
+                               Depends(injectConvolutionNeuralNetworkService)):
+
     try:
+        file = await file.read()
+        print('controller -> cnnBasedImagePredict()')
         predictedClass = convolutionNeuralNetworkService.imagePredict(file)
-        return JSONResponse(content={
-            'predictedClass': predictedClass
-        })
+        return JSONResponse(content={"predictedClass": predictedClass})
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f'잘못된 형식으로 요청을 보냈습니다: {e}')
+        raise HTTPException(status_code=400, detail=f"잘못된 형식으로 요청을 보냈습니다: {e}")
