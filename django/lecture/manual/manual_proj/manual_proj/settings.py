c6c1d5a7244509294255baee96d38d64d9259531
silenc3502 <gcccompil3r@gmail.com>
2024-07-16 20:52:12 +0900

    feat: db host proc

--- django/lecture/manual/manual_proj/manual_proj/settings.py
+++ django/lecture/manual/manual_proj/manual_proj/settings.py
@@ -134,10 +134,10 @@ DATABASES = {
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        # 'HOST': os.getenv('DATABASE_HOST'),
+        'HOST': os.getenv('DATABASE_HOST'),
         # 'PORT': os.getenv('DATABASE_PORT'),
         # 'HOST': 'db',
-        'HOST': 'localhost',
+        # 'HOST': 'localhost',
         'PORT': '3306',
     }
 }
