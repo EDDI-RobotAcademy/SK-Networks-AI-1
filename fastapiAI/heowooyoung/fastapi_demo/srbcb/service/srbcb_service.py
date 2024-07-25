6b0675464a7bc166859da27ea9f313917bf6047e
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-07-25 10:27:27 +0900

    feat: [허우영] 수업 코드 [SKAIPx]

---
+++ fastapiAI/heowooyoung/fastapi_demo/srbcb/service/srbcb_service.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+
+class SrbcbService(ABC):
+    @abstractmethod
+    def predict(self, userSendMessage):
+        pass
---
+++ fastapiAI/heowooyoung/fastapi_demo/srbcb/service/srbcb_service_impl.py
@@ -0,0 +1,9 @@
+from srbcb.repository.srbcb_repository_impl import SrbcbRepositoryImpl
+from srbcb.service.srbcb_service import SrbcbService
+
+class SrbcbServiceImpl(SrbcbService):
+    def __init__(self):
+        self.srbcbRepository = SrbcbRepositoryImpl()
+
+    def predict(self, userSendMessage):
+        return self.srbcbRepository.generateBotMessage(userSendMessage)
