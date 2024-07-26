e6d449038f16a0b7f32eed0f3cf2fd74f6353c94
JeongAram <arongj6317@gmail.com>
2024-07-26 09:08:39 +0900

    fix: [정아람] 중복 파일 삭제

--- django/JeongAram/django_exer4/Product/entity/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    productId = models.AutoField(primary_key=True)
-    productName = models.CharField(max_length=128, null=False)
-    productDescription = models.TextField()
-    productPrice = models.Decimal
