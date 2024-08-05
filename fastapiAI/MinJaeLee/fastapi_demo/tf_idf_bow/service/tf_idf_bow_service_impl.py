2c193301a5692e41911439f3655ffa2d2c689d9d
Meue-L <meue7785@gmail.com>
2024-08-05 22:46:55 +0900

    feat: [MinJaeLee/Meue] 240805 까지 작성한 수업 중 코드입니다. [SKAIPx]

---
+++ fastapiAI/MinJaeLee/fastapi_demo/tf_idf_bow/service/tf_idf_bow_service_impl.py
@@ -0,0 +1,17 @@
+from tf_idf_bow.repository.tf_idf_bow_repository_impl import TfIdfBowRepositoryImpl
+from tf_idf_bow.service.tf_idf_bow_service import TfIdfBowService
+
+
+class TfIdfBowServiceImpl(TfIdfBowService):
+    def __init__(self):
+        self.tfIdfBowRepository = TfIdfBowRepositoryImpl()
+
+    def findSimilarDocumentList(self, userSendMessage):
+        print(f"service -> findSimilarDocumentList()")
+        countVectorizer, countMatrix = self.tfIdfBowRepository.documentVectorization()
+        similarDocumentList = self.tfIdfBowRepository.findSimilar(userSendMessage, countVectorizer, countMatrix)
+        response = [
+            {"document": document, "similarity": score} for document, score in similarDocumentList
+        ]
+
+        return response
