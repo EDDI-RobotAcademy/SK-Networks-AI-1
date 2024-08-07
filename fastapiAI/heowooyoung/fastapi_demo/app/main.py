124c931d275cdf2dcd9b14abc9731acd835587d4
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-08-07 14:23:28 +0900

    feat:[허우영] 2024-08-07 수업 코드 [SKAIPx]

--- fastapiAI/heowooyoung/fastapi_demo/app/main.py
+++ fastapiAI/heowooyoung/fastapi_demo/app/main.py
@@ -19,6 +19,7 @@ from convolution_neural_network.controller.cnn_controller import convolutionNeur
 from exponential_regression.controller.exponential_regression_controller import exponentialRegressionRouter
 from gradient_descent.controller.gradient_descent_controller import gradientDescentRouter
 from kmeans.controller.kmeans_controller import kmeansRouter
+from language_model.controller.language_model_controller import languageModelRouter
 from logistic_regression.controller.logistic_regression_controller import logisticRegressionRouter
 from orders_analysis.controller.orders_analysis_controller import ordersAnalysisRouter
 from polynomialRegression.controller.polynomial_regression_controller import polynomialRegressionRouter
@@ -229,6 +230,7 @@ app.include_router(srbcbRouter)
 app.include_router(sentenceStructureAnalysisRouter)
 app.include_router(tfIdfBowRouter)
 app.include_router(sequenceAnalysisRouter)
+app.include_router(languageModelRouter)
 
 async def testTopicConsume(app: FastAPI):
     consumer = app.state.kafka_test_topic_consumer
@@ -301,4 +303,4 @@ if __name__ == "__main__":
     import uvicorn
 
     # asyncio.run(create_kafka_topics())
-    uvicorn.run(app, host="192.168.0.12", port=33333)
+    uvicorn.run(app, host="192.168.0.3", port=33333)
