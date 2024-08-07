20d43e39ef3ecedf54ce9feedaf92198063a4db2
Yoonseoparkk <patpark97@gmail.com>
2024-08-07 11:32:21 +0900

    feat: [박윤서] 언어 모델링 결과 저장 [SKAIPx]

--- fastapiAI/YoonseoPark/fastapi_demo/language_model/repository/language_model_repository_impl.py
+++ fastapiAI/YoonseoPark/fastapi_demo/language_model/repository/language_model_repository_impl.py
@@ -1,6 +1,12 @@
+import os
+
 import numpy as np
 
 import tensorflow as tf
+from keras import Sequential
+from keras.src.callbacks import ModelCheckpoint
+from keras.src.layers import Embedding, LSTM, Dense
+from keras.src.losses import sparse_categorical_crossentropy
 
 from language_model.repository.language_model_repository import LanguageModelRepository
 
@@ -8,13 +14,21 @@ from language_model.repository.language_model_repository import LanguageModelRep
 class LanguageModelRepositoryImpl(LanguageModelRepository):
     SEQUENCE_LENGTH = 100
 
+    BATCH_SIZE = 64
+    BUFFER_SIZE = 10000
+
+    EMBEDDING_DIM = 256
+    RNN_UNITS = 1024
+
+    EPOCHS = 20
+
     # 고유 문자 목록 생성
     def preprocessForCreateUniqueCharacter(self, text):
         characterList = sorted(list(set(text)))
         charToIndex = {character: index for index, character in enumerate(characterList)}
         indexToChar = np.array(characterList)
 
-        return charToIndex, indexToChar
+        return characterList, charToIndex, indexToChar
 
     def preprocessForCreateTextIndex(self, text, charToIndex):
         return np.array([charToIndex[character] for character in text])
@@ -26,3 +40,34 @@ class LanguageModelRepositoryImpl(LanguageModelRepository):
 
         return examplesForEpoch, characterDataSet, sequenceList
 
+    @staticmethod
+    def __splitInputTarget(chunk):
+        inputText = chunk[:-1]
+        targetText = chunk[1:]
+        return inputText, targetText
+
+    @staticmethod
+    def __customLossFunction(labels, logits):
+        return sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+    def trainModel(self, sequenceList, characterList):
+        dataset = sequenceList.map(LanguageModelRepositoryImpl.__splitInputTarget)
+        shuffledDataSet = dataset.shuffle(self.BUFFER_SIZE).batch(self.BATCH_SIZE, drop_remainder=True)
+
+        vocabularySize = len(characterList)
+
+        model = Sequential([
+            Embedding(vocabularySize, self.EMBEDDING_DIM, batch_input_shape=[self.BATCH_SIZE, None]),
+            LSTM(self.RNN_UNITS, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
+            Dense(vocabularySize)
+        ])
+
+        model.compile(optimizer='adam', loss=LanguageModelRepositoryImpl.__customLossFunction)
+
+        checkpointDirectory = './training_checkpoint'
+        checkpointPrefix = os.path.join(checkpointDirectory, "ckpt_{epoch}")
+        checkpointCallback = ModelCheckpoint(filepath=checkpointPrefix, save_weights_only=True)
+
+        model.fit(shuffledDataSet, epochs=self.EPOCHS, callbacks=[checkpointCallback])
+
+        model.save('shakespeare_model.h5')
