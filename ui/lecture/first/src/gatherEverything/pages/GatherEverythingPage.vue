2518dc9763136aa563ce9a63bb4815e28c23d23d
Sanghoon Lee <gcccompil3r@gmail.com>
2024-08-12 14:51:23 +0900

    feat: [이상훈] Vue + FastAPI + Deep Learning Local Server 연동 준비 [SKAIPx]

---
+++ ui/lecture/first/src/gatherEverything/pages/GatherEverythingPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container>
+        <h2>Deep Learning Local Server Test</h2>
+        <v-btn color="primary" @click="onClickRollDice">딥러닝 학습 시작 버튼</v-btn>
+        <v-btn color="secondary" @click="onClickListDice">딥러닝 추론 요청 버튼</v-btn>
+        <v-btn color="success" @click="onClickGetResult">결과 확인 버튼</v-btn>
+    </v-container>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+const aiCommandModule = 'aiCommandModule'
+
+export default {
+    methods: {
+        ...mapActions(aiCommandModule, [
+            'requestAICommandToFastAPI', 
+            'requestProcessedAICommandResultToFastAPI'
+        ]),
+        onClickRollDice () {
+            console.log("학습 버튼 누름")
+            this.requestAICommandToFastAPI(1)
+        },
+        onClickListDice () {
+            console.log("추론 버튼 누름")
+        },
+        onClickGetResult () {
+            console.log("결과 확인 버튼 누름")
+        },
+    }
+}
+</script>
---
+++ ui/lecture/first/src/gatherEverything/router/GatherEverythingRoutes.ts
@@ -0,0 +1,11 @@
+import GatherEverythingPage from '@/gatherEverything/pages/GatherEverythingPage.vue'
+
+const GatherEverythingRoutes = [
+    {
+        path: '/all-in-one-test',
+        name: 'GatherEverythingPage',
+        component: GatherEverythingPage,
+    },
+]
+
+export default GatherEverythingRoutes
