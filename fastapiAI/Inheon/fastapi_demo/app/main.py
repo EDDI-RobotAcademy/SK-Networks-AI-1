81adc9dab095b8c9f6cd6f9eec0e40948961ef57
Inheon Choi <cih956964@gmail.com>
2024-08-14 14:01:57 +0900

    feat: [최인헌] 0814 수업 코드 [SKAIPx]

--- fastapiAI/Inheon/fastapi_demo/app/main.py
+++ fastapiAI/Inheon/fastapi_demo/app/main.py
@@ -21,6 +21,7 @@ from gradient_descent.controller.gradient_descent_controller import gradientDesc
 from kmeans.controller.kmeans_controller import kmeansRouter
 from language_model.controller.language_model_controller import languageModelRouter
 from logistic_regression.controller.logistic_regression_controller import logisticRegressionRouter
+from openai_basic.controller.openai_basic_controller import openAIBasicRouter
 from orders_analysis.controller.orders_analysis_controller import ordersAnalysisRouter
 from polynomialRegression.controller.polynomial_regression_controller import polynomialRegressionRouter
 from post.controller.post_controller import postRouter
@@ -35,6 +36,7 @@ from srbcb.controller.srbcb_controller import srbcbRouter
 from tf_idf_bow.controller.tf_idf_bow_controller import tfIdfBowRouter
 from tf_iris.controller.tf_iris_controller import tfIrisRouter
 from train_test_evaluation.controller.train_test_evaluation_controller import trainTestEvaluationRouter
+from transition_learning.controller.transition_learning_controller import transitionLearningRouter
 
 
 async def create_kafka_topics():
@@ -233,6 +235,8 @@ app.include_router(sentenceStructureAnalysisRouter)
 app.include_router(sequenceAnalysisRouter)
 app.include_router(languageModelRouter)
 app.include_router(naturalLanguageProcessingRouter)
+app.include_router(transitionLearningRouter)
+app.include_router(openAIBasicRouter)
 
 
 async def testTopicConsume(app: FastAPI):
