e53ec506e4a90f915d96ae24a90a103640ad2da9
Sanghoon Lee <gcccompil3r@gmail.com>
2024-08-21 10:47:18 +0900

    feat: [이상훈] OpenAI 기반 논문 유사도 검색 [SKAIPx]

--- fastapiAI/lecture/fastapi_demo/openai_basic/controller/request_form/openai_paper_similarity_analysis_request_form.py
+++ fastapiAI/lecture/fastapi_demo/openai_basic/controller/request_form/openai_paper_similarity_analysis_request_form.py
@@ -4,3 +4,4 @@ from pydantic import BaseModel
 
 class OpenAIPaperSimilarityAnalysisRequestForm(BaseModel):
     paperTitleList: List[str]
+    userRequestPaperTitle: str
--- fastapiAI/lecture/fastapi_demo/openai_basic/service/openai_basic_service_impl.py
+++ fastapiAI/lecture/fastapi_demo/openai_basic/service/openai_basic_service_impl.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+from openai_basic.controller.response_form.openai_paper_similarity_analysis_response_form import \
+    OpenAIPaperSimilarityAnalysisResponseForm
 from openai_basic.repository.openai_basic_repository_impl import OpenAIBasicRepositoryImpl
 from openai_basic.service.openai_basic_service import OpenAIBasicService
 
@@ -15,10 +19,21 @@ class OpenAIBasicServiceImpl(OpenAIBasicService):
     async def audioAnalysis(self, audioFile):
         return self.__openAiBasicRepository.audioAnalysis(audioFile)
 
-    async def textSimilarityAnalysis(self, paperTitleList):
+    async def textSimilarityAnalysis(self, paperTitleList, userRequestPaperTitle):
         embeddingList = [
             self.__openAiBasicRepository.openAiBasedEmbedding(paperTitle)
             for paperTitle in paperTitleList]
-        # self.__openAiBasicRepository.openAiBasedEmbedding(paperTitleList)
 
-        # return self.__openAiBasicRepository.similarityAnalysis(userSendMessage)
+        embeddingVectorDimension = len(embeddingList[0])
+        faissIndex = self.__openAiBasicRepository.createL2FaissIndex(embeddingVectorDimension)
+        embeddingMatrix = np.array(embeddingList).astype('float32')
+        faissIndex.add(embeddingMatrix)
+
+        indexList, distanceList = (
+            self.__openAiBasicRepository.similarityAnalysis(userRequestPaperTitle, faissIndex))
+
+        print(f"indexList: {indexList}, distanceList: {distanceList}")
+
+        return OpenAIPaperSimilarityAnalysisResponseForm.fromOpenAIPaperSimilarityAnalysis(
+            indexList, distanceList, paperTitleList
+        )
